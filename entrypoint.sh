ccac1e9eaba7c5572e017d0af64f14e7b3b2c14d
Johannes Ott <info@johannes-ott.net>
2021-01-23 11:02:21 +0100

    Bugfix wrong username
    Signed-off-by: Johannes Ott <info@johannes-ott.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 NEW_USER_ID=${USER_ID}
 NEW_GROUP_ID=${GROUP_ID:-$NEW_USER_ID}
 
-echo "Starting with lshs user id: $NEW_USER_ID and group id: $NEW_GROUP_ID"
-if ! id -u lshs >/dev/null 2>&1; then
+echo "Starting with learninghouse user id: $NEW_USER_ID and group id: $NEW_GROUP_ID"
+if ! id -u learninghouse >/dev/null 2>&1; then
   if [ -z "$(getent group $NEW_GROUP_ID)" ]; then
     echo "Create group lshs with id ${NEW_GROUP_ID}"
     groupadd -g $NEW_GROUP_ID learninghouse
   else
     group_name=$(getent group $NEW_GROUP_ID | cut -d: -f1)
-    echo "Rename group $group_name to lshs"
+    echo "Rename group $group_name to learninghouse"
     groupmod --new-name learninghouse $group_name
   fi
   echo "Create user lshs with id ${NEW_USER_ID}"

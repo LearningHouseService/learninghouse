4c56f8696c4f6cdff048f3210de19ae1dc54d675
Johannes Ott <info@johannes-ott.net>
2021-01-23 12:12:16 +0100

    Bugfix log Ausgabe
    Signed-off-by: Johannes Ott <info@johannes-ott.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ NEW_GROUP_ID=${GROUP_ID:-$NEW_USER_ID}
 echo "Starting with learninghouse user id: $NEW_USER_ID and group id: $NEW_GROUP_ID"
 if ! id -u learninghouse >/dev/null 2>&1; then
   if [ -z "$(getent group $NEW_GROUP_ID)" ]; then
-    echo "Create group lshs with id ${NEW_GROUP_ID}"
+    echo "Create group learninghouse with id ${NEW_GROUP_ID}"
     groupadd -g $NEW_GROUP_ID learninghouse
   else
     group_name=$(getent group $NEW_GROUP_ID | cut -d: -f1)
     echo "Rename group $group_name to learninghouse"
     groupmod --new-name learninghouse $group_name
   fi
-  echo "Create user lshs with id ${NEW_USER_ID}"
+  echo "Create user learninghouse with id ${NEW_USER_ID}"
   adduser -u $NEW_USER_ID --disabled-password --gecos '' --home "${LHS_HOME}" --gid $NEW_GROUP_ID learninghouse
 fi
 

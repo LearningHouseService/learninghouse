871367b3024411f9a5ad2e3f8681c6b8b566542a
Johannes Ott <mail@johannes-ott.net>
2021-10-06 21:10:16 +0200

    Add version information to logging
    Signed-off-by: Johannes Ott <mail@johannes-ott.net>

--- learninghouse/service.py
+++ learninghouse/service.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, __version__ as flask_version
 from flask_restful import Api
 from paste.translogger import TransLogger
 from waitress import serve
+
+from sklearn import __version__ as skl_version
+from pandas import __version__ as pd_version
+from numpy.version import version as np_version
 
 from . import __version__, logger
 from .model import ModelAPI, ModelPrediction, ModelTraining
@@ -15,9 +19,13 @@ api.add_resource(ModelAPI, '/info/<string:model>')
 
 
 def run(production, host, port):
+    logger.info('Running LearningHouse service % s', __version__)
+    logger.info('Libraries scikit-learn (%s), pandas(%s), numpy(%s), flask(%s)',
+                skl_version, pd_version, np_version, flask_version)
+
     if production:
-        logger.info('Running learninghouse %s in production mode', __version__)
+        logger.info('Running in production mode')
         serve(TransLogger(app, logger=logger), host=host, port=port)
     else:
-        logger.info('Running learninghouse %s in development mode', __version__)
+        logger.info('Running in development mode')
         app.run(host=host, port=port, debug=False)

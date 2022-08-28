f536bc9394fab1fb8e4d90653b7046d4015f0f1d
Johannes Ott <mail@johannes-ott.net>
2022-08-28 22:27:58 +0200

    feat(version): Add angular version
    Signed-off-by: Johannes Ott <mail@johannes-ott.net>

--- src/app/shared/services/app.service.ts
+++ src/app/shared/services/app.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
-import { VERSION } from 'src/environments/version';
+import { Injectable, VERSION as CORE_VERSION } from '@angular/core';
+import { VERSION as APP_VERSION } from 'src/environments/version';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-  get_version(): string {
-    return VERSION.semverString;
+  get_app_version(): string {
+    return APP_VERSION.semverString;
+  }
+
+  get_angular_version(): string {
+    return CORE_VERSION.full
   }
 }

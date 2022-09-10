161795bfee126edc72b0a83b6398ce345805c9ec
Johannes Ott <mail@johannes-ott.net>
2022-09-10 21:34:19 +0200

    feat(sidenav): Open status saved in sessionStorage
    Signed-off-by: Johannes Ott <mail@johannes-ott.net>

--- src/app/modules/layout/components/sidenav/sidenav.service.ts
+++ src/app/modules/layout/components/sidenav/sidenav.service.ts
@@ -7,8 +7,15 @@ import { BehaviorSubject } from 'rxjs';
 export class SidenavService {
   isOpened$ = new BehaviorSubject<boolean>(false);
 
+  constructor() {
+    const isOpened = JSON.parse(sessionStorage.getItem('sidenav_is_opened') || 'false') === true;
+    this.isOpened$.next(isOpened);
+  }
+
   toggleNavigation(): void {
-    this.isOpened$.next(!this.isOpened$.getValue());
+    const nextOpened = !this.isOpened$.getValue()
+    this.isOpened$.next(nextOpened);
+    sessionStorage.setItem('sidenav_is_opened', JSON.stringify(nextOpened))
   }
 
 }

c05fdb706c62228ce1ac03abd418a46c485934e1
Johannes Ott <mail@johannes-ott.net>
2023-04-28 19:22:37 +0000

    python >=3.10 required
    Signed-off-by: Johannes Ott <mail@johannes-ott.net>

--- core/setup.py
+++ core/setup.py
@@ -11,53 +11,41 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.readlines()
 
 
-packages = setuptools.find_packages(exclude=['tests'])
-packages.append('learninghouse.static')
-packages.append('learninghouse.static.docs')
-if path.exists('learninghouse/ui'):
-    packages.append('learninghouse.ui')
-    packages.append('learninghouse.ui.assets')
-    packages.append('learninghouse.ui.assets.fonts')
-    packages.append('learninghouse.ui.assets.i18n')
+packages = setuptools.find_packages(exclude=["tests"])
+packages.append("learninghouse.static")
+packages.append("learninghouse.static.docs")
+if path.exists("learninghouse/ui"):
+    packages.append("learninghouse.ui")
+    packages.append("learninghouse.ui.assets")
+    packages.append("learninghouse.ui.assets.fonts")
+    packages.append("learninghouse.ui.assets.i18n")
 
 setuptools.setup(
-    name='learninghouse',
+    name="learninghouse",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='learningHouse - Teach your smart home everything',
+    description="learningHouse - Teach your smart home everything",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/LearningHouseService/learninghouse-monorepo',
-    author='Johannes Ott',
-    author_email='info@johannes-ott.net',
+    long_description_content_type="text/markdown",
+    url="https://github.com/LearningHouseService/learninghouse-monorepo",
+    author="Johannes Ott",
+    author_email="info@johannes-ott.net",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-
-        'Topic :: Home Automation',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Framework :: FastAPI',
-
-        'Natural Language :: English',
-
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Home Automation",
+        "License :: OSI Approved :: MIT License",
+        "Framework :: FastAPI",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
     ],
-    keywords='smart home, machine learning, house automation',
+    keywords="smart home, machine learning, house automation",
     packages=packages,
     include_package_data=True,
-    python_requires='>=3.7, <4',
+    python_requires=">=3.10, <4",
     install_requires=[req for req in requirements if req[:2] != "# "],
-    entry_points={
-        'console_scripts': [
-            'learninghouse=learninghouse.service:run'
-        ]
-    },
+    entry_points={"console_scripts": ["learninghouse=learninghouse.service:run"]},
     project_urls={
-        'Bug Reports': 'https://github.com/LearningHouseService/learninghouse-monorepo/issues'
-    }
+        "Bug Reports": "https://github.com/LearningHouseService/learninghouse/issues"
+    },
 )
